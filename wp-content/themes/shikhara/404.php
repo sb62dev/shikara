d0ef7a16c11a6d055ae4bb8901abcd5ec1c2ae13
sb62dev <sb62dev@gmail.com>
2024-09-30 22:32:10 +0530

    updated 404 page ui
    updated 404 page ui

--- wp-content/themes/shikhara/404.php
+++ wp-content/themes/shikhara/404.php
@@ -13,22 +13,23 @@
 get_header(); ?>
 
 <div class="wrap">
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentyseventeen' ); ?></h1>
-				</header><!-- .page-header -->
-				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentyseventeen' ); ?></p>
-
-					<p> <a href="/" class="btn-style"> Back to homepage </a></p>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-		</main><!-- #main -->
-	</div><!-- #primary -->
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main">
+            <div class="container_outer container_outer_pb">
+                <section class="error-404 not-found padd-row">
+                    <div class="container text-center">
+                        <header class="page-header">
+                            <h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentyseventeen' ); ?></h1>
+                        </header><!-- .page-header -->
+                        <div class="page-content">
+                            <p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentyseventeen' ); ?></p>
+                            <p class="mt-4"> <a href="/" class="btn-style"> Back to homepage </a></p>
+                        </div>
+                    </div><!-- .page-content -->
+                </section><!-- .error-404 -->
+            </div>
+        </main><!-- #main -->
+    </div><!-- #primary -->
 </div><!-- .wrap -->
 
 <?php
